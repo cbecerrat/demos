cb04cc5ba89bf509e894db539e8f99ee63c3f8ce
Cesar <cesarbe@mx1.ibm.com>
2017-01-31 10:30:29 -0600

    DEV

--- company/src/main/java/beans/Hello.java
+++ company/src/main/java/beans/Hello.java
@@ -3,6 +3,7 @@ package beans;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
@@ -11,29 +12,41 @@ import domain.Product;
 import repositories.ProductRepository;
 
 @Controller
-public class Hello {
-	private int calledCounter = 0; 
+public class Hello {	
 	@Autowired
 	ProductRepository pr;
-	@RequestMapping({"/","/index"})	
-	public ModelAndView index() {							
-		ModelAndView model = new ModelAndView("index");		
-		return model;		
+
+	@RequestMapping(value = { "/", "/index" }, method = RequestMethod.GET)	
+	public ModelAndView index() {
+		System.out.println("Here.");
+		ModelAndView model = new ModelAndView("index");
+		return model;
 	}
-	
-	@RequestMapping(value={"/","/index"}, params={"id"})	
-	public ModelAndView index(@RequestParam(value = "id", required=false) long id) {			
-		ModelAndView model = new ModelAndView("index");		
-		return model;		
-	}
-	
+
 	@RequestMapping("/getProduct")
 	@ResponseBody
-	public String sayHello(ModelAndView mav, @RequestParam(value = "id") long id) {			
+	public String sayHello(ModelAndView mav, @RequestParam(value = "id") long id) {
 		Product product = pr.findOne(id);
-		if(null == product){
+		if (null == product) {
 			product = new Product();
-		}		
+		}
 		return product.getProductAsJSON();
-	}		
+	}
+
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
+	public ModelAndView login(@RequestParam(value = "error", required = false) String error,
+			@RequestParam(value = "logout", required = false) String logout) {
+
+		ModelAndView model = new ModelAndView();
+		if (error != null) {
+			model.addObject("error", "Invalid username and password!");
+		}
+
+		if (logout != null) {
+			model.addObject("msg", "You've been logged out successfully.");
+		}
+		model.setViewName("login");
+
+		return model;
+	}
 }
