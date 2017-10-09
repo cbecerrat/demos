b81dfb30de15dc4c1526718ee6a7872fffb63250
Cesar <cesarbe@mx1.ibm.com>
2017-10-09 14:19:43 -0500

    https2

--- partyonline/src/main/java/config/SecurityConfig.java
+++ partyonline/src/main/java/config/SecurityConfig.java
@@ -26,6 +26,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
+		http.requiresChannel().anyRequest().requiresSecure();
 		http.authorizeRequests().antMatchers("/servicing/**").access("hasRole('ROLE_ADMIN')")
 			.and()
 				.formLogin().loginPage("/login").failureUrl("/login?error").usernameParameter("username").passwordParameter("password")
